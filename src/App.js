055273cd67427a7d85dd18c7cf8c37a6f974eeb2
Thiago <bodnarthiago@gmail.com>
2022-03-10 18:31:53 -0300

    requisito 4 feito

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
       <main>
-        <Route path="/" component={ Search } exact />
+        <BrowserRouter>
+          <Route path="/" component={ Search } exact />
+        </BrowserRouter>
       </main>
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { getCategories } from '../services/api';
+import CategorieButton from './CategorieButton';
+import './Search.css';
 
 export default class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: true,
+    }, async () => {
+      const result = await getCategories();
+      this.setState({
+        categories: result,
+        loading: false,
+      });
+    });
+  }
+
   render() {
+    const { categories, loading } = this.state;
     return (
       <div>
         <form>
@@ -13,6 +37,17 @@ export default class Search extends React.Component {
           Digite
           algum termo de pesquisa ou escolha uma categoria.
         </h2>
+        <nav className="categories-container">
+          {
+            loading === true ? <p>Carregando...</p>
+              : categories.map((categorie) => (
+                <CategorieButton
+                  key={ categorie.id }
+                  name={ categorie.name }
+                  id={ categorie.id }
+                />))
+          }
+        </nav>
       </div>
     );
   }
