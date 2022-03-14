8fd50d98081d5b4bbf87691293a8c29f28d56ab8
Thiago <bodnarthiago@gmail.com>
2022-03-14 18:53:25 -0300

    requisito 8 feito

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -9,8 +9,10 @@ class ShoppingCart extends React.Component {
         {
           cartList.length > 0 ? cartList.map((item, index) => (
             <div key={ index }>
-              <p data-testid="shopping-cart-product-name">{ item }</p>
-              <input type="number" data-testid="shopping-cart-product-quantity" value="1" />
+              <p data-testid="shopping-cart-product-name">{ item.title }</p>
+              <p data-testid="shopping-cart-product-quantity">
+                { item.count }
+              </p>
             </div>
           ))
             : <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
@@ -21,7 +23,7 @@ class ShoppingCart extends React.Component {
 }
 
 ShoppingCart.propTypes = {
-  cartList: PropTypes.arrayOf(PropTypes.string).isRequired,
+  cartList: PropTypes.arrayOf(PropTypes.any).isRequired,
 };
 
 export default ShoppingCart;
