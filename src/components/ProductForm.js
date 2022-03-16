fb593532dc8257c2f19ab4b7b32bf88a3cf6ba80
Leo Araujo <leonardo_02452@hotmail.com>
2022-03-16 09:57:26 -0300

    Requisito 11 ainda em andamento

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class ProductForm extends Component {
   render() {
+    const { onClickButton, onInputChange } = this.props;
     return (
       <form>
-        <input type="email" data-testid="product-detail-email" placeholder="E-mail" />
-        <input type="radio" data-testid="1-rating" value="1" />
-        <input type="radio" data-testid="2-rating" value="2" />
-        <input type="radio" data-testid="3-rating" value="3" />
-        <input type="radio" data-testid="4-rating" value="4" />
-        <input type="radio" data-testid="5-rating" value="5" />
+        <input
+          type="email"
+          data-testid="product-detail-email"
+          placeholder="E-mail"
+          onChange={ onInputChange }
+        />
+        <label htmlFor="rating">
+          <input type="radio" name="rating" data-testid="1-rating" value="1" />
+          <input type="radio" name="rating" data-testid="2-rating" value="2" />
+          <input type="radio" name="rating" data-testid="3-rating" value="3" />
+          <input type="radio" name="rating" data-testid="4-rating" value="4" />
+          <input type="radio" name="rating" data-testid="5-rating" value="5" />
+        </label>
         <textarea
           data-testid="product-detail-evaluation"
           placeholder="Mensagem (opcional)"
+          onChange={ onInputChange }
         />
-        <button type="submit" data-testid="submit-review-btn">Avaliar</button>
+        <button
+          type="submit"
+          data-testid="submit-review-btn"
+          onClick={ onClickButton }
+        >
+          Avaliar
+        </button>
       </form>
     );
   }
 }
+
+ProductForm.propTypes = {
+  onClickButton: PropTypes.func.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+};
