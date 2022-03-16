0fe5dec40c301b222b1f2282b2e7dd31302786e7
Leo Araujo <leonardo_02452@hotmail.com>
2022-03-16 11:22:49 -0300

    Requisito 11 feito

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -10,19 +10,51 @@ export default class ProductForm extends Component {
           type="email"
           data-testid="product-detail-email"
           placeholder="E-mail"
+          name="email"
           onChange={ onInputChange }
         />
         <label htmlFor="rating">
-          <input type="radio" name="rating" data-testid="1-rating" value="1" />
-          <input type="radio" name="rating" data-testid="2-rating" value="2" />
-          <input type="radio" name="rating" data-testid="3-rating" value="3" />
-          <input type="radio" name="rating" data-testid="4-rating" value="4" />
-          <input type="radio" name="rating" data-testid="5-rating" value="5" />
+          <input
+            type="radio"
+            name="rating"
+            data-testid="1-rating"
+            value="1"
+            onChange={ onInputChange }
+          />
+          <input
+            type="radio"
+            name="rating"
+            data-testid="2-rating"
+            value="2"
+            onChange={ onInputChange }
+          />
+          <input
+            type="radio"
+            name="rating"
+            data-testid="3-rating"
+            value="3"
+            onChange={ onInputChange }
+          />
+          <input
+            type="radio"
+            name="rating"
+            data-testid="4-rating"
+            value="4"
+            onChange={ onInputChange }
+          />
+          <input
+            type="radio"
+            name="rating"
+            data-testid="5-rating"
+            value="5"
+            onChange={ onInputChange }
+          />
         </label>
         <textarea
           data-testid="product-detail-evaluation"
           placeholder="Mensagem (opcional)"
           onChange={ onInputChange }
+          name="evaluation"
         />
         <button
           type="submit"
